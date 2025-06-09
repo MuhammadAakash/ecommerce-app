9cfe8fc08723535b67356447529aed07a7361f9c
Mohammad Aakash <muhammadaakash249@gmail.com>
2025-06-09 13:40:56 +0100

    Whole app of Ecommerce app

---
+++ src/store/config.tsx
@@ -0,0 +1,11 @@
+import { Platform } from "react-native"
+
+//FOR EMULATOR OR SIMULATOR DEVICE ⬇️
+export const BASE_URL = Platform.OS == 'android' ?
+    'http://10.0.2.2:3000' :
+  'http://192.168.29.254:3000'
+
+
+//FOR PHYSICAL DEVICE ⬇️
+// USE YOUR NETWORK IP OR HOSTED URL
+// export const BASE_URL = 'http://192.168.29.88:3000'
